d272860f5ad58991f6bb00484f595da236b87a30
Timur Vafin <timurv@gmail.com>
2008-04-12 02:09:03 +0400

    TextileFu plugin should be loaded only of RedCloth loaded

--- lib/active_project/rails_plugins/textilize_fu/init.rb
+++ lib/active_project/rails_plugins/textilize_fu/init.rb
@@ -1,3 +1,10 @@
-require File.join(File.dirname(__FILE__), 'lib', 'textilize_fu')
+begin
+    require_library_or_gem "redcloth" unless Object.const_defined?(:RedCloth)
+    File.join(File.dirname(__FILE__), 'lib', 'textilize_fu')    
+    
+    ActiveRecord::Base.send :include, TextilizeFu
+rescue LoadError
+end
 
-ActiveRecord::Base.send :include, TextilizeFu
+
+
