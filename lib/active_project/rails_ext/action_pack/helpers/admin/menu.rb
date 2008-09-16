bc74560944e7a0ba63c9598fe71ac8b549c0a0a4
Timur Vafin <timur.vafin@flatsoft.com>
2008-09-16 17:02:21 +0400

    Disable restrict_to int the menu helper

--- lib/active_project/rails_ext/action_pack/helpers/admin/menu.rb
+++ lib/active_project/rails_ext/action_pack/helpers/admin/menu.rb
@@ -6,25 +6,25 @@ module ActiveProject #:nodoc:
                     module Menu
                         def menu_top(menu = 'admin')
                             ::Menu.new(menu).show do |item|
-                                restrict_to(item.roles) do
+#                                restrict_to(item.roles) do
                                     if active_top_menu.to_s == item.key.to_s
                                         content_tag('li', link_to(item.title, item.url), :class => 'here')
                                     else
                                         content_tag('li', link_to(item.title, item.url))
                                     end 
-                                end
+#                                end
                             end
                         end
 
                         def menu_sub(menu = 'admin')
                             ::Menu.new(menu).show(active_top_menu) do |item|
-                                restrict_to(item.roles) do
+#                                restrict_to(item.roles) do
                                     if active_sub_menu == item.key
                                         content_tag('li', link_to(item.title, item.url), :class => 'here')
                                     else
                                         content_tag('li', link_to(item.title, item.url))
                                     end 
-                                end
+#                                end
                             end
                         end
                     end
